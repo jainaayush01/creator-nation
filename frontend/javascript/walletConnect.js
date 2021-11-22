06d5a5e3a6d5709351e6cf9c3e87ed13079bd727
mbcse <mbcse50@gmail.com>
2021-11-22 23:48:49 +0530

    Matic and wallet connect upgraded

--- frontend/javascript/walletConnect.js
+++ frontend/javascript/walletConnect.js
@@ -1,14 +1,21 @@
 //  Create WalletConnect Provider
 const provider = new WalletConnectProvider.default({
   infuraId: "e992178cee28442e86aa8c6611d7d472",
+  rpc: {
+    80001: "https://matic-mumbai.chainstacklabs.com"
+  },
+  
 });
+console.log(provider);
 
 document.getElementById("btn-connect").addEventListener("click", async()=>{
   //  Enable session (triggers QR Code modal)
   try{
     await provider.enable();
+    console.log(provider);
     //  Create Web3 instance
     window.web3 = new Web3(provider);
+    sign()
     document.getElementById("connected").style.display="block";
     document.getElementById("prepare").style.display="none";
   }catch(err){
@@ -41,3 +48,15 @@ document.getElementById("btn-disconnect").addEventListener("click", async()=>{
   }catch(err){
   }
 })
+
+async function sign(){
+  if(provider.connected){
+    document.getElementById("connected").style.display="block";
+    document.getElementById("prepare").style.display="none";
+    console.log(provider);
+      // const chainId = await window.web3.eth.chainId();
+      // console.log(chainId);
+      const signedMessage = await provider.request("sign","Hello To Creator Nation");
+      console.log(signedMessage);
+  }
+}
