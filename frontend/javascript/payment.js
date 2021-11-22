0468243410aa913e198748bd5c794b9bd9c77bf3
Aayush Jain <imjainaayush@gmail.com>
2021-11-22 09:40:30 +0530

    stripe integration

---
+++ frontend/javascript/payment.js
@@ -0,0 +1,125 @@
+const BACKEND_URL = 'http://localhost:4242';
+const STRIPE_PUBLISHABLE_KEY = 'pk_test_51JbPGjSDIAUQ0zT7QrDZqVpznfVEDLAxQc0iM2lLdXpJ1bZMsdGkkNFW9sOpbeWuKNBqIYVERhc6zEoZDDfTuQUu00nUonCHip';
+
+// utils
+// Helper for displaying status messages.
+const addMessage = (message) => {
+    const messagesDiv = document.querySelector('#messages');
+    messagesDiv.style.display = 'block';
+    const messageWithLinks = addDashboardLinks(message);
+    messagesDiv.innerHTML = `> ${messageWithLinks}<br>`;
+    console.log(`Debug: ${message}`);
+};
+
+// Adds links for known Stripe objects to the Stripe dashboard.
+const addDashboardLinks = (message) => {
+    const piDashboardBase = 'https://dashboard.stripe.com/test/payments';
+    return message.replace(
+        /(pi_(\S*)\b)/g,
+        `<a href="${piDashboardBase}/$1" target="_blank">$1</a>`
+    );
+};
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    // Load the publishable key from the server. The publishable key
+    // is set in your .env file.
+    // const {publishableKey} = await fetch(`${BACKEND_URL}/config`).then((r) => r.json());
+    // if (!publishableKey) {
+    //   addMessage(
+    //     'No publishable key returned from the server. Please check `.env` and try again'
+    //   );
+    //   alert('Please set your Stripe publishable API key in the .env file');
+    // }
+
+    const publishableKey = STRIPE_PUBLISHABLE_KEY;
+
+    const stripe = Stripe(publishableKey, {
+        apiVersion: '2020-08-27',
+    });
+
+    const appearance = {
+        theme: 'stripe',
+        variables: {
+            colorPrimary: '#00e',
+            colorBackground: '#000',
+            colorText: '#30313d',
+            colorDanger: '#df1b41',
+            fontFamily: 'Ideal Sans, system-ui, sans-serif',
+            spacingUnit: '2px',
+            borderRadius: '4px',
+        }
+    }
+
+    const elements = stripe.elements();
+    console.log(elements);
+    const card = elements.create('card');
+    card.mount('#card-element');
+
+    // When the form is submitted...
+    const form = document.getElementById('payment-form');
+    let submitted = false;
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        // Disable double submission of the form
+        if (submitted) { return; }
+        submitted = true;
+        form.querySelector('#submit').disabled = true;
+
+        // Make a call to the server to create a new
+        // payment intent and store its client_secret.
+        const { error: backendError, clientSecret } = await fetch(
+            `${BACKEND_URL}/create-payment-intent`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    currency: 'inr',
+                    paymentMethodType: 'card',
+                }),
+            }
+        ).then((r) => r.json());
+
+        if (backendError) {
+            addMessage(backendError.message);
+
+            // reenable the form.
+            submitted = false;
+            form.querySelector('button').disabled = false;
+            return;
+        }
+
+        // addMessage(`Client secret returned.`);
+
+        const nameInput = document.querySelector('#name');
+
+        // Confirm the card payment given the clientSecret
+        // from the payment intent that was just created on
+        // the server.
+        const { error: stripeError, paymentIntent } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: nameInput.value,
+                    },
+                },
+            }
+        );
+
+        if (stripeError) {
+            addMessage(stripeError.message);
+
+            // reenable the form.
+            submitted = false;
+            form.querySelector('#submit').disabled = false;
+            return;
+        }
+
+        addMessage(`Payment ${paymentIntent.status}: ${paymentIntent.id}`);
+    });
+});
