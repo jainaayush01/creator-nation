4bac33f8b5bb6e3682864f1d63d07271b48373c5
Aayush Jain <imjainaayush@gmail.com>
2021-11-25 22:39:36 +0530

    fix: mint only owner to public

--- Smart Contract/hardhat.config.js
+++ Smart Contract/hardhat.config.js
@@ -8,7 +8,10 @@ require("@nomiclabs/hardhat-waffle")
 module.exports = {
   solidity: "0.8.3",
   networks: {
-
+    localhost: {
+      url: 'http://localhost:7545',
+      accounts: [process.env.PRIVATEKEY]
+    },
     mumbai: {
       url: 'https://polygon-mumbai.g.alchemy.com/v2/f-suHXrnx-xJMb_UM1ZLIl3TR6Q0XtvP',
       accounts: [process.env.PRIVATEKEY]
