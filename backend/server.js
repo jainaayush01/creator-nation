e7c7ce0fe81eb69e5c2189ba2f8e12757f3357ba
Aayush Jain <imjainaayush@gmail.com>
2021-11-28 18:42:47 +0530

    server push

--- backend/server.js
+++ backend/server.js
@@ -4,41 +4,79 @@ const cors = require('cors');
 const { ethers } = require('ethers');
 require('dotenv').config();
 
-var privateKey = process.env.LOCAL_PRIVATE_KEY;
+var privateKey = process.env.MATIC_PRIVATE_KEY;
+const NFT = require('./artifacts/contracts/NFT.sol/CreatorNation.json');
+const PriceFeed = require('./artifacts/contracts/ChainlinkPriceFeed.sol/MaticPrice.json');
+const NFTAddress = process.env.NFT_ADDRESS;
+const PriceFeedAddress = process.env.PRICE_FEED_ADDRESS;
 
-const MATICprovider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545');
-const wallet = new ethers.Wallet(privateKey, MATICprovider);
+let contract
+async function callME() {
+  const provider = new ethers.providers.JsonRpcProvider(process.env.MATIC_JSON_RPC)
+  const ownerWallet = new ethers.Wallet(privateKey, provider)
+  console.log(ownerWallet)
+  const signer = await provider.getSigner()
+  console.log(signer);
+  // const signerAddress = await signer.getAddress()
+  // console.log(signerAddress)
+
+  contract = new ethers.Contract(NFTAddress, NFT.abi, ownerWallet)
+  // console.log(contract.signer.address);
+}
+
+
+
+
+// const MATICprovider = new ethers.providers.JsonRpcProvider(process.env.MATIC_JSON_RPC);
+// const wallet = new ethers.Wallet(privateKey, MATICprovider);
 // console.log(wallet.address);
 // console.log(wallet.provider);
 // console.log(wallet.publicKey);
 
-const NFT = require('../Smart Contract/artifacts/contracts/NFT.sol/CreatorNation.json'); // pls change accordingly
-const ProceFeed = require('../Smart Contract/artifacts/contracts/ChainlinkPriceFeed.sol/MaticPrice.json');
-const NFTAddress = process.env.NFT_ADDRESS;
-const PriceFeedAddress = process.env.PRICE_FEED_ADDRESS;
-console.log(wallet.provider);
 
-let NFTContract = new ethers.Contract(NFTAddress, NFT.abi, wallet);
-let PriceFeedContract = new ethers.Contract(PriceFeedAddress, ProceFeed.abi, wallet);
-console.log(wallet);
+// console.log(wallet);
+
+// let NFTContract = new ethers.Contract(NFTAddress, NFT.abi, wallet);
+// let PriceFeedContract = new ethers.Contract(PriceFeedAddress, ProceFeed.abi, wallet);
+// console.log(wallet);
 // console.log(NFTContract.address);
 // console.log(PriceFeedContract.address);
 
 // console.log(NFTContract.functions.contractURI);
+
 async function foo() {
   const res = await NFTContract.functions.contractURI();
   console.log(res);
 }
-// foo();
 
 const handlePaymentIntentSucceeded = async (paymentIntent, userEthAddress, tokenId, tokenNos) => {
-  // await foo();
-  const res = await NFTContract.functions.buyTokensFiat(userEthAddress, ethers.BigNumber.from("1"), ethers.BigNumber.from(tokenNos), {
-    gasLimit: 500000,
-    gasPrice: 20000,
-  });
-  return res;
+  try {
+    // console.log({ paymentIntent, userEthAddress, tokenId, tokenNos });
+    await callME();
+    const txn = await contract.buyTokensFiat(userEthAddress, parseInt(tokenId), 1);
+    console.log(txn);
+    txn.wait();
+    console.log(txn);
+    return txn;
+  }
+  catch (err) {
+    return err;
+  }
 }
+
+const handleCreateTokens = async (creatorAddress, tokenName, tokenPrice, tokenSupplyLimit, uri, mediaUrl) => {
+  try {
+    await callME();
+    const txn = await contract.mint(creatorAddress, tokenName, tokenPrice, tokenSupplyLimit, uri, mediaUrl);
+    txn.wait();
+    console.log(txn);
+    return txn;
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
 
 
 app.use(cors());
@@ -72,12 +110,25 @@ app.get('/config', (req, res) => {
   });
 });
 
+app.post('/create-tokens', async (req, res) => {
+  const { creatorAddress, tokenName, tokenPrice, tokenSupplyLimit, uri, mediaUrl } = req.body;
+  try {
+    const txn = await handleCreateTokens(creatorAddress, tokenName, tokenPrice, tokenSupplyLimit, uri, mediaUrl);
+    res.send(txn);
+  }
+  catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+})
+
 app.post('/create-payment-intent', async (req, res) => {
-  const { paymentMethodType, amount, currency, userEthAddress, tokenId } = req.body;
+  const { paymentMethodType, amount, currency, userEthAddress, tokenId, productType } = req.body;
+  console.log({paymentMethodType, amount, currency, userEthAddress, tokenId, productType})
 
   const params = {
     payment_method_types: [paymentMethodType],
-    amount: amount,
+    amount: amount*100*80,
     currency: currency,
   }
 
@@ -94,11 +145,13 @@ app.post('/create-payment-intent', async (req, res) => {
 
   try {
     const paymentIntent = await stripe.paymentIntents.create(params);
-    const result = await handlePaymentIntentSucceeded(paymentIntent, userEthAddress, tokenId, tokenNos = 1);
-    console.log(result);
+    if (productType == "1") {
+      console.log('he')
+      const txn = await handlePaymentIntentSucceeded(paymentIntent, userEthAddress, tokenId, tokenNos = 1);
+      console.log(txn);
+    }
     res.send({
-      clientSecret: paymentIntent.client_secret,
-      res
+      clientSecret: paymentIntent.client_secret
     });
   } catch (e) {
     return res.status(400).send({
@@ -110,7 +163,7 @@ app.post('/create-payment-intent', async (req, res) => {
 });
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
-app.post('/webhook', express.raw({type: 'application/json'}), async (req, res) => {
+app.post('/webhook', express.raw({ type: 'application/json' }), async (req, res) => {
   const sig = req.headers['stripe-signature'];
 
   let event;
@@ -139,7 +192,7 @@ app.post('/webhook', express.raw({type: 'application/json'}), async (req, res) =
       break;
     default:
       console.log(`Unhandled event type ${event.type}`);
-      // console.log(event)
+    // console.log(event)
   }
 
   res.json({ received: true });
